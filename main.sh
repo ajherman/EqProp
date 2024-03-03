61a98c8bf172a13f3e122f4c8765facafb0099c4
Ari Herman <ari.j.herman@gmail.com>
2024-03-02 21:16:28 -0800

    wip

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 
 #SBATCH --job-name=main
 #SBATCH --time 10:00:00
-#SBATCH -N 6
+#SBATCH -N 10
 #SBATCH -p shared-gpu
 #module load miniconda3
 #source activate /vast/home/ajherman/miniconda3/envs/pytorch
@@ -35,6 +35,7 @@ do
 for tau_dynamic in {0.1,0.2,0.4}
 do
 for beta in {0.1,0.2,0.4}
+do
 stdp0_dir=compare_stdp0_omega_"$omega"_tau_"$tau_dynamic"_h1_"$hidden_size1"_h2_"$hidden_size2"_lr1_"$lr1"_lr2_"$lr2"_lr3_"$lr3"_beta_"$beta"
 stdp1_dir=compare_stdp1_omega_"$omega"_tau_"$tau_dynamic"_h1_"$hidden_size1"_h2_"$hidden_size2"_lr1_"$lr1"_lr2_"$lr2"_lr3_"$lr3"_beta_"$beta"
 stdp2_dir=compare_stdp2_omega_"$omega"_tau_"$tau_dynamic"_h1_"$hidden_size1"_h2_"$hidden_size2"_lr1_"$lr1"_lr2_"$lr2"_lr3_"$lr3"_beta_"$beta"
@@ -55,6 +56,7 @@ srun -N 1 -n 1 -c $cores -o "$stdp2_dir".out --open-mode=append ./main_wrapper.s
 srun -N 1 -n 1 -c $cores -o "$stdp3_dir".out --open-mode=append ./main_wrapper.sh --M 1 --spiking --load --use-time-variables --directory $stdp3_dir --omega $omega --step $step --spike-method poisson --tau-dynamic $tau_dynamic --tau-trace 0.2 --action train --batch-size $batch_size --activation-function hardsigm --size_tab 10 $hidden_size1 $hidden_size2 784 --lr_tab $lr1 $lr2 $lr3 --epochs $epochs --T1 $T1 --T2 $T2 --beta $beta --cep --learning-rule stdp --update-rule stdp &
 # srun -N 1 -n 1 -c $cores -o "$stdp4_dir".out --open-mode=append ./main_wrapper.sh --M 1 --spiking --load --use-time-variables --directory $stdp4_dir --omega $omega --step $step --spike-method poisson --tau-dynamic $tau_dynamic --tau-trace 0.4 --action train --batch-size $batch_size --activation-function hardsigm --size_tab 10 $hidden_size1 $hidden_size2 784 --lr_tab $lr1 $lr2 $lr3 --epochs $epochs --T1 $T1 --T2 $T2 --beta $beta --cep --learning-rule stdp --update-rule stdp &
 # srun -N 1 -n 1 -c $cores -o "$stdp5_dir".out --open-mode=append ./main_wrapper.sh --M 1 --spiking --load --use-time-variables --directory $stdp5_dir --omega $omega --step $step --spike-method poisson --tau-dynamic $tau_dynamic --tau-trace 0.8 --action train --batch-size $batch_size --activation-function hardsigm --size_tab 10 $hidden_size1 $hidden_size2 784 --lr_tab $lr1 $lr2 $lr3 --epochs $epochs --T1 $T1 --T2 $T2 --beta $beta --cep --learning-rule stdp --update-rule stdp &
+done
 done
 done
 done
